6fbd5222cabef8d0610883eda2dd95d4ae8c04f7
Charles SAYAMATH <charles.sayamath@etu.unilim.fr>
2020-12-19 16:40:01 +0100

    Sphere

--- SphereAnalytique.py
+++ SphereAnalytique.py
@@ -6,31 +6,31 @@ import numpy as np
 import sys
 import math
 
-img_width = 100
-img_height = 100
-img_deep = 100
+img_width = 50
+img_height = 50
+img_depth = 50
 
 def draw( circle_center,circle_radius,voxels,width):
     voxels = np.array(draw_sphere(circle_center,circle_radius,voxels,img_width))
-
+ 
     fig = plt.figure()
     ax = fig.gca(projection='3d')
+    ax.set_box_aspect(aspect=[1,1,1])   
     ax.voxels(voxels, facecolors='red')
-
     plt.show()
     return
 
 def draw_sphere(circle_center,circle_radius,voxels,width):
-
     w1 = (circle_radius)*(circle_radius)
     w2 = (circle_radius + 1)*(circle_radius+1)
 
-    for z in range (img_deep):
+    for z in range (img_depth):
+        pos_z = z - circle_center[2]
+
         for y in range (img_height):
+            pos_y = y - circle_center[1]
             for x in range (img_width):
                 pos_x = x - circle_center[0]
-                pos_y = y - circle_center[1]
-                pos_z = z - circle_center[2]
                 dist =pos_x*pos_x + pos_y*pos_y + pos_z*pos_z
 
                 if w1 <= dist < w2 :
@@ -43,7 +43,7 @@ def main():
     # création d'une fenêtre de taille 100x100x100
 
 
-    voxels = [[[False for _ in range(100)] for _ in range(100)] for _ in range(100)]
+    voxels = [[[False for _ in range(img_width)] for _ in range(img_height)] for _ in range(img_depth)]
 
     print("Centre de la sphere ?")
     center_x = int(input("x : "))
